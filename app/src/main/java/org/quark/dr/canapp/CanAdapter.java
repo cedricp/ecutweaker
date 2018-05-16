d3857ce8654a9dc439253492b9ee7e33e5b10083
Cedric PAILLE <cedricpaille@gmail.com>
2018-05-16 22:46:39 +0200

    Improvements

--- app/src/main/java/org/quark/dr/canapp/CanAdapter.java
+++ app/src/main/java/org/quark/dr/canapp/CanAdapter.java
@@ -20,21 +20,23 @@ public class CanAdapter {
     private DeluxeSpeedView mRpmView, mSpeedView;
     private Thread          socketThread;
     private long            mSpeedMemory, mRpmMemory, mOdometerMemory, mOilLevelMemory;
-    private long            mWaterTempMemory, mFuelLevelMemory;
-    private long            mFuelConsumptionMemory, mFuelTimestampMemory, mLockMemory;
+    private long            mWaterTempMemory, mFuelLevelMemory, mExternalTempMemory;
+    private long            mLockMemory;
+    private long[]          mTimeStamps;
+    byte                    mLastFuelConsumptionMemory;
     volatile private boolean socketThreadRunning = true;
 
     private static class SafeHandler extends Handler {
         private final WeakReference<CanAdapter> mAdapter;
         public SafeHandler(CanAdapter adapter) {
-            mAdapter = new WeakReference<CanAdapter>(adapter);
+            mAdapter = new WeakReference<>(adapter);
         }
 
         @Override
         public void handleMessage(Message msg) {
             CanAdapter adapter = mAdapter.get();
             if (adapter == null){
-                Log.i("CanApp", "Weakref issue...");
+                Log.i(TAG, "Weakref issue...");
                 return;
             }
 
@@ -43,22 +45,24 @@ public class CanAdapter {
             long cants = frame.getTimeStamp();
             byte[] data = frame.getData();
             if (canaddr == 0x0354) {
+                long time = cants - adapter.mTimeStamps[0];
+                adapter.mTimeStamps[0] = cants;
                 int vehicleSpeed = data[1] & 0xFF;
                 vehicleSpeed |= (data[0] & 0xFF) << 8;
 
                 if (vehicleSpeed != adapter.mSpeedMemory) {
-                    adapter.mSpeedView.speedTo((float)vehicleSpeed * 0.01f, 200);
+                    adapter.mSpeedView.speedTo((float)vehicleSpeed * 0.01f, time);
                     adapter.mSpeedMemory = vehicleSpeed;
                 }
             } else if (canaddr == 0x0715){
-                int odometer = data[0] & 0xFF << 16;
-                odometer |= data[1] & 0xFF << 8;
-                odometer |= data[0] & 0xFF;
+                long odometer = (data[0] & 0xFF) << 16;
+                odometer |= (data[1] & 0xFF) << 8;
+                odometer |= data[2] & 0xFF;
                 int oillevel = (data[3] & 0xFF) & 0b00111100;
                 int fuellevel = (data[4] & 0b11111110) >> 1;
 
                 if (odometer != adapter.mOdometerMemory){
-                    adapter.mOdometerView.setText(String.valueOf(odometer) + "KM");
+                    adapter.mOdometerView.setText(String.valueOf(odometer) + " KM");
                     adapter.mOdometerMemory = odometer;
                 }
                 if (oillevel != adapter.mOilLevelMemory){
@@ -70,39 +74,52 @@ public class CanAdapter {
                     adapter.mFuelLevelMemory = fuellevel;
                 }
             } else if (canaddr == 0x0551){
+                long time = cants - adapter.mTimeStamps[1];
                 int waterTemp = data[0] & 0xFF;
-                long fuelConsumption = data[1] & 0xFF;
+                byte fuelConsumption = data[1];
 
                 if (waterTemp != adapter.mWaterTempMemory){
                     adapter.mWaterTempView.speedTo(waterTemp - 40, 3000);
                     adapter.mWaterTempMemory = waterTemp;
                 }
-                if (fuelConsumption != adapter.mFuelConsumptionMemory){
-                    float seconds = abs(cants - adapter.mFuelTimestampMemory) * 0.001f;
-                    float mm3 = abs(fuelConsumption - adapter.mFuelConsumptionMemory) * 80;
+
+                if (time > 400) {
+                    float seconds = time * 0.001f;
+                    byte  diff = (byte)(fuelConsumption - adapter.mLastFuelConsumptionMemory);
+                    int   idiff = diff & 0xFF;
+                    float mm3 = (float)idiff * 80.f;
                     float mm3perheour = (mm3 / seconds) * 3600.f;
                     float dm3perhour = mm3perheour * 0.000001f;
+                    adapter.mLastFuelConsumptionMemory = fuelConsumption;
+                    adapter.mTimeStamps[1] = cants;
 
-                    if (adapter.mSpeedMemory > 2000){
-                        float dm3per100kmh = (dm3perhour / ((float)adapter.mSpeedMemory));
-                        String fuelstring = String.format("%.1f", dm3per100kmh) + " L/100";
+                    if (adapter.mSpeedMemory > 2000) {
+                        float dm3per100kmh = (dm3perhour / ((float) adapter.mSpeedMemory));
+                        String fuelstring = String.format("%.2f", dm3per100kmh) + " L/100";
+                        adapter.mFuelConsumptionView.setText(fuelstring);
                     } else {
-                        String fuelstring = String.format("%.1f", dm3perhour) + " L/h";
+                        String fuelstring = String.format("%.2f", dm3perhour) + " L/h";
                         adapter.mFuelConsumptionView.setText(fuelstring);
                     }
-                    adapter.mFuelTimestampMemory = cants;
-                    adapter.mFuelConsumptionMemory = fuelConsumption;
                 }
             } else if (canaddr == 0x0181) {
-                int rpm = (data[1] & 0xFF) << 8;
-                rpm |= data[0] & 0xFF;
+                long time = cants - adapter.mTimeStamps[2];
+                adapter.mTimeStamps[2] = cants;
+                int rpm = (data[0] & 0xFF) << 8;
+                rpm |= data[1] & 0xFF;
 
                 if (rpm != adapter.mRpmMemory){
-                    adapter.mRpmView.speedTo((rpm * 0.125f), cants);
+                    adapter.mRpmView.speedTo((rpm * 0.000125f), time);
                     adapter.mRpmMemory = rpm;
                 }
             } else if (canaddr == 0x060D) {
                 int globallock = data[2] & 0b00011000;
+                int externaltemp = data[4] & 0xFF;
+
+                if (externaltemp != adapter.mExternalTempMemory){
+                    adapter.mExternalTempView.setText(String.valueOf(externaltemp - 40) + " °C");
+                    adapter.mExternalTempMemory = externaltemp;
+                }
 
                 if (globallock != adapter.mLockMemory){
                     boolean bootlock = (globallock & 0b00001000) == 0b00001000;
@@ -114,7 +131,7 @@ public class CanAdapter {
     }
     CanAdapter(MainActivity activity){
         mMainActivity = activity;
-
+        mTimeStamps         = new long[10];
         mWaterTempView      = activity.findViewById(R.id.waterTempView);
         mFuelLevelView      = activity.findViewById(R.id.fuelLevelView);
         mOilView            = activity.findViewById(R.id.oilLevelView);
@@ -130,11 +147,11 @@ public class CanAdapter {
             public void run() {
                 while (true) {
                     try {
-                        Log.i("CanApp", "interface binding...");
+                        Log.i(TAG, "interface binding...");
                         CanSocket canSockAdapter = new CanSocket(CanSocket.Mode.RAW);
                         CanSocket.CanInterface caninterface = new CanSocket.CanInterface(canSockAdapter, "can0");
                         canSockAdapter.bind(caninterface, 0x00, 0x00);
-                        Log.i("CanApp", "interface bound : " + caninterface.toString());
+                        Log.i(TAG, "interface bound : " + caninterface.toString());
                         while (true) {
                             CanSocket.CanFrame frame = canSockAdapter.recv();
                             Message message = handler.obtainMessage();
@@ -142,16 +159,16 @@ public class CanAdapter {
                             handler.sendMessage(message);
 
                             if (Thread.currentThread().isInterrupted() || !socketThreadRunning) {
-                                Log.i("CanApp", "Thread stop");
+                                Log.i(TAG, "Thread stop");
                                 return;
                             }
                         }
                     } catch (Exception e) {
-                        Log.i("CanApp", "interface error " + e.getMessage());
+                        Log.i(TAG, "interface error " + e.getMessage());
                     }
 
                     if (Thread.currentThread().isInterrupted() || !socketThreadRunning) {
-                        Log.i("CanApp", "Thread stop");
+                        Log.i(TAG, "Thread stop");
                         return;
                     }
 
@@ -168,7 +185,6 @@ public class CanAdapter {
         socketThread.setPriority(Thread.MIN_PRIORITY);
     }
 
-
     public void shutdown(){
         socketThread.interrupt();
         socketThreadRunning = false;
@@ -177,6 +193,6 @@ public class CanAdapter {
         } catch (Exception e) {
 
         }
-        Log.i("CanApp", "App stop");
+        Log.i(TAG, "App stop");
     }
 }
